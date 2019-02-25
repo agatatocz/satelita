e74e272151dde12c0534d7c9f3a34c98d01dfa9e
Agata <agatatocz@gmail.com>
2019-02-25 11:37:35 +0100

    added basic content in Measurement component

--- src/components/Measurement.jsx
+++ src/components/Measurement.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
+import "../styles/Measurement.scss";
 
-const Measurement = () => {
-  return <div>Measurement</div>;
-};
+class Measurement extends Component {
+  state = {
+    seconds: 1
+  };
+
+  setSeconds = seconds => {
+    seconds = Number(seconds);
+    this.setState({ seconds });
+  };
+
+  render() {
+    return (
+      <div className="measurement">
+        <h4>
+          Aby wyznaczyć prędkość Międzynarodowej Stacji Kosmicznej, należy
+          dokonać dwóch odczytów jej położenia.
+        </h4>
+        <div className="spans">
+          <span>Wybierz ilość sekund pomiędzy odczytami:</span>
+          <span style={{ fontWeight: "bold" }}>{this.state.seconds}s</span>
+        </div>
+        <input
+          type="range"
+          min="1"
+          max="60"
+          value={this.state.seconds}
+          onChange={e => this.setSeconds(e.currentTarget.value)}
+          className="seconds-slider"
+        />
+        <button>START</button>
+      </div>
+    );
+  }
+}
 
 export default Measurement;
