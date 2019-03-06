d24a1c2e5424b3eec8d5e1a2848993dd72980149
Agata <agatatocz@gmail.com>
2019-03-06 18:49:01 +0100

    added popup message box in results section

--- src/components/ResultsSection/ResultsSection.jsx
+++ src/components/ResultsSection/ResultsSection.jsx
@@ -9,6 +9,17 @@ const ResultsSection = ({ locations, results }) => {
   return (
     <div className="results container-div">
       <h4>Twoje pomiary</h4>
+      <div className="popup">
+        <i className="fa fa-question-circle-o" aria-hidden="true" />
+        <span className="popup-info">
+          Czas pomiędzy pomiarami może różnić się od czasu wybranego przez
+          Ciebie za pomocą suwaka z uwagi na opóźnienia występujące w ruchu
+          sieciowym. Ilość sekund obliczana jest na postawie znaczników
+          czasowych informujących, w którym momencie wartości odczytanych
+          współrzędnych geograficznych satelity były aktualne.
+        </span>
+      </div>
+
       <div className="results-list">
         {locations.length ? null : (
           <p>Wykonaj pierwsze pomiary klikając "START"!</p>
